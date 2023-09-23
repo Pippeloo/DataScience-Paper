ebe1d2521babeac6c423e1fa0cbd2daf3bac6991
Jules Torfs <julestorfs@amps.be>
2023-09-23 17:30:04 -0400

    saving

--- DescisionTree/statistics/memoryAndCpu.py
+++ DescisionTree/statistics/memoryAndCpu.py
@@ -22,14 +22,14 @@ def predict():
     # Loop the specified number of times
     for i in range(10):
         # Make a prediction for the specified features.
-        model.predict([[5, 1.5], [3, 2]])
+        model.predict([[263, 62, 0.01, 71.27962362, 65, 1154, 19.1, 83, 6, 8.16, 65, 0.1, 584.25921, 33736494, 17.2, 17.3,0.479, 10.1]])
         # Add the current time measurement to the list
         time_measurements.append(time.time() - current_time)
         # Get the CPU usage
         usage = p.cpu_percent(interval=None)
         # Add the CPU usage measurement to the list
         cpu_usage_measurements.append(usage)
-        # time.sleep(0.01)
+        time.sleep(0.1)
 
 # Call the function to make predictions
 predict()
